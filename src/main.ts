4ce5d60dfe97cbed56ed77b857043e282ccc36c7
Yang <84857233+zengyang0428@users.noreply.github.com>
2022-08-29 17:00:38 +0800

    Delete main.ts

--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/css/reset.less'
-import Card from './components/Card/index.vue'
-createApp(App).component('Card',Card).mount('#app')
