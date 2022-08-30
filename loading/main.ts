ebabbf1ead3e71c83d79b203082ab4212072b337
Yang <84857233+zengyang0428@users.noreply.github.com>
2022-08-30 21:40:23 +0800

    Update main.ts

--- loading/main.ts
+++ loading/main.ts
@@ -1,26 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import mitt from "mitt";
 import Loading from './components/loading'
-const Mit = mitt();
+app.use(Loading)
 
 //TypeScript注册
 // 由于必须要拓展ComponentCustomProperties类型才能获得类型提示
-declare module "vue" {
-  export interface ComponentCustomProperties {
-    $Bus: typeof Mit;
-  }
-}
-
-const app = createApp(App);
-app.use(Loading)
-
-type Filter = {
-  format: <T>(srt: T) => string;
-};
 declare module "@vue/runtime-core" {
   export interface ComponentCustomProperties {
-    $filters:Filter,
     $loading:{
       show:()=>void,
       hide:()=>void
@@ -29,7 +15,6 @@ declare module "@vue/runtime-core" {
 }
 
 //Vue3挂载全局API
-app.config.globalProperties.$Bus = Mit;
 app.config.globalProperties.$filters = {
   format<T>(str: T): string {
     return `真${str}`;
