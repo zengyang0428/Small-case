9738a1fc9bed6b0480d12072b48cc2ad51166d08
Yang <84857233+zengyang0428@users.noreply.github.com>
2022-08-30 21:43:32 +0800

    Update index1.vue

--- loading/index1.vue
+++ loading/index1.vue
@@ -6,6 +6,7 @@
 
 <script setup lang="ts">
 import { ComponentInternalInstance } from 'vue'
+//考虑到在获取上下文和全局挂载实例的时候会用到这个getCurrentInstance  获取全局挂载实例
 const { appContext } = getCurrentInstance() as ComponentInternalInstance
 const showLoading = () => {
 	console.log(appContext);
