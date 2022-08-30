b4a075a72c7ba98d422c9e041406ee107fc2e0a8
Yang <84857233+zengyang0428@users.noreply.github.com>
2022-08-30 21:31:15 +0800

    Update 小窗口移动.vue

--- 小窗口移动.vue
+++ 小窗口移动.vue
@@ -8,22 +8,30 @@
 <script setup lang="ts">
 import { Directive, DirectiveBinding } from 'vue';
 const vMove: Directive<any, void> = (el: HTMLElement) => {
+	//获取节点
 	let moveElement: HTMLDivElement = el.firstElementChild as HTMLDivElement
 	console.log(moveElement);
+	//接口指用户与指针设备( 如鼠标 )交互时发生的事件
 	const mouseDown = (e: MouseEvent) => {
+		//获取当前的位置
 		let X = e.clientX - el.offsetLeft
 		let Y = e.clientY - el.offsetTop
+		//接口指用户与指针设备( 如鼠标 )交互时发生的事件
 		const move = (e: MouseEvent) => {
 			console.log(e);
+			//获取移动的位置 - 获取当前的位置
 			el.style.left = e.clientX - X + 'px'
 			el.style.top = e.clientY - Y + 'px'
 
 		}
+		//鼠标移动
 		document.addEventListener('mousemove', move)
+		//当我们在元素上松开鼠标时，mouseup 会触发。
 		document.addEventListener('mouseup', () => {
 			document.removeEventListener('mousemove', move)
 		})
 	}
+	//当我们在元素上按下鼠标时，mousedown 会触发
 	moveElement.addEventListener('mousedown', mouseDown)
 
 }
