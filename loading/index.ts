a372c2e7bc31ca73781bda59112ee161ef082d14
Yang <84857233+zengyang0428@users.noreply.github.com>
2022-08-30 21:42:23 +0800

    Update index.ts

--- loading/index.ts
+++ loading/index.ts
@@ -5,7 +5,9 @@ import Loading from './index.vue'
 
 export default{
 	install(app:App){
+		//创建虚拟DOM
 		const vnode:VNode = createVNode(Loading)
+		//创建真实DOM
 		render(vnode,document.body)
 		app.config.globalProperties.$loading={
 			show:vnode.component?.exposed?.show,
